399079d504ce993b3dcf7a373b5f7beb11c9ae95
Rajan <krajan92946@gmail.com>
2025-01-18 15:48:27 +0530

    Palindrome

---
+++ PracticeSet1/palindrome.java
@@ -0,0 +1,11 @@
+
+
+
+public class palindrome{
+    public static void main(String[] args)
+    {
+         String str="madam";
+         String reverse=new StringBuilder(str).toString();
+         System.out.println(str.equals(reverse));
+    }
+}
