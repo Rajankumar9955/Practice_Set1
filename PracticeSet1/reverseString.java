29460f090447584e85c55d3c564e80a85533ce3f
Rajan <krajan92946@gmail.com>
2025-01-18 15:43:24 +0530

    Revese String

---
+++ PracticeSet1/reverseString.java
@@ -0,0 +1,10 @@
+
+
+// Reverse String
+public class reverseString{
+    public static void main(String[] args){
+        String str="AutoCorrect";
+        StringBuilder reverse=new StringBuilder(str).reverse();
+        System.out.println(reverse);
+    }
+}
