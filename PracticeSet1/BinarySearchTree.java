62e97db2a18f5006a618da49d758bbff5e1e1739
Rajan <krajan92946@gmail.com>
2025-01-21 17:29:27 +0530

    Binary search Tree

---
+++ PracticeSet1/BinarySearchTree.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class BinarySearchTree {
+   
+  	public static void preOrder( Node root ) {
+      
+    	if( root == null)
+        	return;
+      
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+     
+    }
+
+ /* Node is defined as :
+ class Node 
+    int data;
+    Node left;
+    Node right;
+    
+    */
+
+	public static Node insert(Node root,int data) {
+         if (root == null) {
+            return 0;
+        }
+       
+        if (root.data == val) {
+            return 1;
+        } else if (val < root.data) {
+            return isPresent(root.left, val);
+        } else {
+            return isPresent(root.right, val);
+        }
+    
+
+    	
+    }
+
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+      	preOrder(root);
+    }	
+}
